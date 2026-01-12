4b86506eede20da236daa1b724add154f80204b5
Gasskin <415753928@qq.com>
2026-01-12 23:33:00 +0800

    GAS

---
+++ Assets/Script/Editor/SkillTimeline/SkillActionClip.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+[System.Serializable]
+public class SkillActionClip: PlayableAsset, ITimelineClipAsset
+{
+    public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+    {
+        var playable = ScriptPlayable<SkillActionBehaviour>.Create(graph);
+        // 获取 Behaviour 实例并赋值
+        var behaviour = playable.GetBehaviour();
+        return playable;
+    }
+
+    public ClipCaps clipCaps => ClipCaps.None;
+}
---
+++ Assets/Script/Editor/SkillTimeline/SkillActionTrack.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine.Timeline;
+
+[TrackColor(1f, 1f, 0f)] 
+[TrackClipType(typeof(SkillActionClip))]
+public class SkillActionTrack : TrackAsset
+{
+    
+}
